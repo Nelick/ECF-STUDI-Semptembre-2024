d280dc602a4ef023b91843c71377795e4761de41
Nelick <maximilian.champroux@gmail.com>
2024-09-19 11:38:39 +0200

    Update Front-end
    Mise place des Pages !

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script>
 	import Navbar from './element/navbar.svelte';
+	import Header from './element/header.svelte';
 </script>
 
 <div class="container">
 	<div class="navbar">
 		<Navbar />
 	</div>
-	<div class="main"></div>
+	<div class="header">
+		<Header />
+	</div>
 </div>
 
 <style>
 	.container {
+    padding: 5em;
 		background-image: url('gamestore-background.jpeg');
 		margin: 0;
 		line-height: normal;
